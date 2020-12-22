cc3fe7bc5795f5814b49398f485219279ef44dde
lelo451 <ra98995@uem.br>
2020-12-22 20:28:42 -0300

    Stage 1/5: Arrangement

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -3,6 +3,35 @@ package cinema;
 public class Cinema {
 
     public static void main(String[] args) {
-        // Write your code here
+        int rows = 8;
+        int cols = 9;
+        int rowCounter = 1;
+        int colCounter = 1;
+
+        String[][] matrix = new String[8][9];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 && j == 0) {
+                    matrix[i][j] = " ";
+                } else if (j == 0) {
+                    matrix[i][j] = String.format("%d", rowCounter);
+                    rowCounter++;
+                } else if (i == 0) {
+                    matrix[i][j] = String.format("%d", colCounter);
+                    colCounter++;
+                } else {
+                    matrix[i][j] = "S";
+                }
+            }
+        }
+
+        System.out.println("Cinema:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
