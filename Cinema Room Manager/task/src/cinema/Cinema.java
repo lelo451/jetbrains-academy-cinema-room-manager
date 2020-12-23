7fc5aaed73878839e8fd9ef54837e58c390d6c84
lelo451 <ra98995@uem.br>
2020-12-22 21:20:33 -0300

    Stage 3/5: Tickets

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -11,17 +11,93 @@ public class Cinema {
         int rows = sc.nextInt();
         System.out.print("Enter the number of seats in each row: ");
         int seatsPerRow = sc.nextInt();
-        System.out.print("Total income: ");
+
+        String[][] matrix;
+        matrix = fillMatrix(rows, seatsPerRow);
+
+        printSeatsInScreenRoom(matrix, rows, seatsPerRow);
+
+        System.out.print("Enter a row number: ");
+        int rowNum = sc.nextInt();
+        System.out.print("Enter a seat number in that row: ");
+        int seatNum = sc.nextInt();
+
+        int price = price(rows, seatsPerRow, rowNum);
+
+        System.out.printf("Ticket price: $%d\n", price);
+
+        printSeatsInScreenRoom(bookSeatInMatrix(matrix, rowNum, seatNum), rows, seatsPerRow);
+    }
+
+    public static int price(int rows, int seatsPerRow, int rowNum) {
+        int price;
+        if (rows * seatsPerRow <= 60) {
+            price = 10;
+        } else {
+            if (rows % 2 == 0 && rowNum <= rows / 2) {
+                price = 10;
+            } else if (rows % 2 == 0 && rowNum > rows / 2) {
+                price = 8;
+            } else if (rows % 2 != 0 && rowNum <= (rows - 1) / 2) {
+                price = 10;
+            } else {
+                price = 8;
+            }
+        }
+        return price;
+    }
+
+    public static void incomeCalculator(int rows, int seatsPerRow) {
+        int income = 0;
         if (rows * seatsPerRow <= 60) {
             System.out.println("$" + (rows * seatsPerRow * 10));
         } else {
             if (rows % 2 == 0) {
-                int income = rows / 2 * seatsPerRow * 18;
+                income = rows / 2 * seatsPerRow * 18;
                 System.out.println("$" + income);
             } else {
-                int income = ((rows - 1) / 2) * seatsPerRow * 10 + (((rows - 1) / 2) + 1) * seatsPerRow * 8;
+                income = ((rows - 1) / 2) * seatsPerRow * 10 + (((rows - 1) / 2) + 1) * seatsPerRow * 8;
                 System.out.println("$" + income);
             }
         }
+    }
+
+    public static String[][] fillMatrix(int rows, int seatsPerRow) {
+        int rowCounter = 1;
+        int colCounter = 1;
+
+        String[][] matrix = new String[rows + 1][seatsPerRow + 1];
+
+        for (int i = 0; i < rows + 1; i++) {
+            for (int j = 0; j < seatsPerRow + 1; j++) {
+                if (i == 0 && j == 0) {
+                    matrix[i][j] = " ";
+                } else if (j == 0) {
+                    matrix[i][j] = String.format("%d", rowCounter);
+                    rowCounter++;
+                } else if (i == 0) {
+                    matrix[i][j] = String.format("%d", colCounter);
+                    colCounter++;
+                } else {
+                    matrix[i][j] = "S";
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static void printSeatsInScreenRoom(String[][] matrix, int rows, int seatsPerRow) {
+        System.out.println("Cinema:");
+        for (int i = 0; i < rows + 1; i++) {
+            for (int j = 0; j < seatsPerRow + 1; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static String[][] bookSeatInMatrix(String[][] matrix, int rowNumber, int seatNum) {
+        matrix[rowNumber][seatNum] = "B";
+        return matrix;
     }
 }
